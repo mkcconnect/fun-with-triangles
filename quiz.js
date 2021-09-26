b169da2a98db7ce6ba8f5ed82068e285e4c62f29
sloth-grrr <35689080+sloth-grrr@users.noreply.github.com>
2021-09-26 16:56:30 +0530

    finished quiz

---
+++ quiz.js
@@ -0,0 +1,19 @@
+const quizForm = document.querySelector(".quiz-form");
+const submitAnswerBtn = document.querySelector("#submit-answer-btn");
+const outputEl = document.querySelector("#output");
+
+const correctAnswers = ["90", "right angled"];
+
+function calculateScore() {
+    let score = 0;
+    let index = 0;
+    const formResults = new FormData(quizForm);
+    for(let entry of formResults.entries()){
+        if (value === correctAnswers[index]){
+            score = score + 1;
+        }
+        index = index + 1;
+    }
+}
+
+submitAnswerBtn.addEventListener("click", calculateScore);
