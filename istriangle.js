01a1c2db0958bde57be27786ddc74653021ac306
sloth-grrr <35689080+sloth-grrr@users.noreply.github.com>
2021-09-26 11:38:05 +0530

    added program logic

--- istriangle.js
+++ istriangle.js
@@ -1,13 +1,21 @@
 const inputs = document.querySelectorAll(".angle-input");
-const isTriangleBtn = document.querySelector("#is-triangle.btn");
+const isTriangleBtn = document.querySelector("#is-triangle-btn");
 const outputEl = document.querySelector("#output");
 
 function calculateSumOfAngles(angle1, angle2, angle3) {
     const sumOfAngles = angle1 + angle2 + angle3;
+    return sumOfAngles;
 }
 
 function isTriangle() {
-    const sumOfAngles = calculateSumOfAngles(45, 45, 90);
+    const sumOfAngles = calculateSumOfAngles(Number(inputs[0].value), Number(inputs[1].value), Number(inputs[2].value));
+    
+    if(sumOfAngles === 180){
+        outputEl.innerText = "Yes, it is a triangle.";
+    } 
+    else {
+        outputEl.innerText = "No, it is not a triangle.";
+    }
 }
 
 isTriangleBtn.addEventListener("click", isTriangle)
