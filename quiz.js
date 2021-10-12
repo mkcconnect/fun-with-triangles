cf7f407f01fdd29946b5513ea0f4494d47089457
sloth-grrr <35689080+mkcconnect@users.noreply.github.com>
2021-10-12 13:14:25 +0530

    fixed quiz

--- quiz.js
+++ quiz.js
@@ -2,18 +2,18 @@ const quizForm = document.querySelector(".quiz-form");
 const submitAnswerBtn = document.querySelector("#submit-answer-btn");
 const outputEl = document.querySelector("#output");
 
-const correctAnswers = ["90", "right angled"];
+const correctAnswers = ["90", "Right angled", "Equilateral Triangle"];
 
 function calculateScore() {
     let score = 0;
     let index = 0;
     const formResults = new FormData(quizForm);
-    for(let entry of formResults.entries()){
+    for(let value of formResults.values()){
         if (value === correctAnswers[index]){
             score = score + 1;
-        }
+        } 
         index = index + 1;
     }
+    outputEl.innerText = "Score: " + score;
 }
-
 submitAnswerBtn.addEventListener("click", calculateScore);
